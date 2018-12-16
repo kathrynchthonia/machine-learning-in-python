86a1a8d83494f9254c922e54ed02e91668bfe86d
twheelertech <tony@wheelertechconsulting.com>
2018-12-16 13:53:01 -0600

    Completed SVR

---
+++ Part 2 - Regression/Section 7 - Support Vector Regression (SVR)/svr.py
@@ -0,0 +1,49 @@
+# Regression Template
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('Position_Salaries.csv')
+X = dataset.iloc[:, 1:2].values
+y = dataset.iloc[:, 2].values
+
+# Splitting the dataset into the Training set and Test set
+"""from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)"""
+
+# Feature Scaling
+from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X = sc_X.fit_transform(X)
+sc_y = StandardScaler()
+y = sc_y.fit_transform(y)
+
+# Fitting the SVR to the dataset
+# Create your regressor here
+from sklearn.svm import SVR
+regressor = SVR(kernel='rbf')
+regressor.fit(X, y)
+
+# Predicting a new result
+y_pred = regressor.predict(6.5)
+
+# Visualising the SVR results
+plt.scatter(X, y, color = '#db24b1')
+plt.plot(X, regressor.predict(X), color = 'indigo')
+plt.title('Truth or Bluff (Regression Model)')
+plt.xlabel('Position level')
+plt.ylabel('Salary')
+plt.show()
+
+# Visualising the SVR results (for higher resolution and smoother curve)
+"""X_grid = np.arange(min(X), max(X), 0.1)
+X_grid = X_grid.reshape((len(X_grid), 1))
+plt.scatter(X, y, color = '#db24b1')
+plt.plot(X_grid, regressor.predict(X_grid), color = 'indigo')
+plt.title('Truth or Bluff (Regression Model)')
+plt.xlabel('Position level')
+plt.ylabel('Salary')
+plt.show()"""
