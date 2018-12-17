7efa215f06bcf81ce55e71de8bd801c44ef282c2
twheelertech <tony@wheelertechconsulting.com>
2018-12-17 02:10:59 -0600

    Completed PCA

--- Part 7 - Natural Language Processing/natural_language_processing.py
+++ Part 7 - Natural Language Processing/natural_language_processing.py
@@ -31,8 +31,15 @@ X = cv.fit_transform(corpus).toarray()
 y = dataset.iloc[:, 1].values
 
 
-# Fitting Naive Bayes to the Training set
+# Splitting the dataset into the Training set and Test set
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.20, random_state = 0)
 
+
+# Fitting Naive Bayes to the Training set
+from sklearn.naive_bayes import GaussianNB
+classifier = GaussianNB()
+classifier.fit(X_train, y_train)
 
 # Predicting the Test set results
 y_pred = classifier.predict(X_test)
@@ -40,3 +47,4 @@ y_pred = classifier.predict(X_test)
 # Making the Confusion Matrix
 from sklearn.metrics import confusion_matrix
 cm = confusion_matrix(y_test, y_pred)
+(55+91)/200
