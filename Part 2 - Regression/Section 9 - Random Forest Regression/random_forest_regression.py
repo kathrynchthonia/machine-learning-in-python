e9b54a0d82b7448c159be9c2c2a7a6c88e72255c
twheelertech <tony@wheelertechconsulting.com>
2018-12-16 20:11:04 -0600

    CompletedClustering and Classification

--- Part 2 - Regression/Section 9 - Random Forest Regression/random_forest_regression.py
+++ Part 2 - Regression/Section 9 - Random Forest Regression/random_forest_regression.py
@@ -1,5 +1,7 @@
 # Random Forest Regression
 
+# Regression Template
+
 # Importing the libraries
 import numpy as np
 import matplotlib.pyplot as plt
@@ -11,7 +13,7 @@ X = dataset.iloc[:, 1:2].values
 y = dataset.iloc[:, 2].values
 
 # Splitting the dataset into the Training set and Test set
-"""from sklearn.cross_validation import train_test_split
+"""from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)"""
 
 # Feature Scaling
@@ -22,19 +24,20 @@ X_test = sc_X.transform(X_test)
 sc_y = StandardScaler()
 y_train = sc_y.fit_transform(y_train)"""
 
-# Fitting Random Forest Regression to the dataset
+# Fitting the Random Forest Regression to the dataset
 from sklearn.ensemble import RandomForestRegressor
-regressor = RandomForestRegressor(n_estimators = 10, random_state = 0)
+regressor = RandomForestRegressor(n_estimators = 300, random_state = 0)
 regressor.fit(X, y)
 
 # Predicting a new result
 y_pred = regressor.predict(6.5)
 
-# Visualising the Random Forest Regression results (higher resolution)
+
+# Visualising the Regression results (for higher resolution and smoother curve)
 X_grid = np.arange(min(X), max(X), 0.01)
 X_grid = X_grid.reshape((len(X_grid), 1))
-plt.scatter(X, y, color = 'red')
-plt.plot(X_grid, regressor.predict(X_grid), color = 'blue')
+plt.scatter(X, y, color = '#db24b1')
+plt.plot(X_grid, regressor.predict(X_grid), color = 'indigo')
 plt.title('Truth or Bluff (Random Forest Regression)')
 plt.xlabel('Position level')
 plt.ylabel('Salary')
